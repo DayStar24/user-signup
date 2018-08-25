fcdb4518a574a66e6575e6555c498bd9c8fef76d
zedboson <walters.ralph@outlook.com>
2018-08-24 22:37:26 -0500

    Formatted embeded Jinja for readability

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from flask import Flask , request , redirect , render_template
 import re , cgi , os , jinja2
 
-templates_dir = os.path.join(os.path.dirname(__file__),'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(templates_dir), autoescape=True)
+templates_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(templates_dir), autoescape = True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 #route and handler of the original form
-@app.route("/")
+@app.route('/', methods = ['GET'])
 def index():
     return render_template('index.html')
 
-@app.route("/user_signup", methods=['POST'])
+@app.route('/user_signup', methods = ['POST'] )
 def validate_form():
 	template = ''
 	username = request.form['username']
@@ -25,40 +25,41 @@ def validate_form():
 	error_verify_password = '' 
 	error_email = '' 
 	
-	if not re.match(r'^(?=.{8,})',username):
+	if username == '':
 		error_username = 'Missing username'
 	
-	if not re.match(r'^(?=.*[a-z])',password):
+	if not re.match(r'^(?=.*[a-z])', password):
 		error_password += 'Need lowercase letter'
-	if not re.match(r'^(?=.*[A-Z])',password):
+	if not re.match(r'^(?=.*[A-Z])', password):
 		error_password += '  |  Need uppercase letter'
-	if not re.match(r'^(?=.*[0-9])',password):
+	if not re.match(r'^(?=.*[0-9])', password):
 		error_password += '  |  Need a number'
-	if not re.match(r'^(?=.*[^A-z0-9])',password):
+	if not re.match(r'^(?=.*[^A-z0-9])', password):
 		error_password += '  |  Need special character'
-	if not re.match(r'^(?=.{8,})',password):
+	if not re.match(r'^(?=.{8,})', password):
 		error_password += '  |  Need at least 8 characters'
 	
 	if verify_password != password:
 		error_verify_password = 'Password verification failed'
 		
-	if (email) and (not re.match(r'^(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)',email)):
+	if (email) and ( not re.match(r'^(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)', email) ):
 		error_email = 'Invalid email'
 	
 	if (error_username != '') or (error_password != '') or (error_email != ''):
 		template = render_template(
 			'index.html', 
-			error_username=error_username, 
-			error_password=error_password, 
-			error_email=error_email, 
-			username=username, 
-			email=email
+			error_username = error_username, 
+			error_password = error_password,
+			error_verify_password = error_verify_password,
+			error_email = error_email, 
+			username = username, 
+			email = email
 		)
 	else:
 		template = render_template(
 			'/welcome.html', 
-			username=username, 
-			email=email
+			username = username, 
+			email = email
 		)
 	
 	return template
